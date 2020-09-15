946e0be19ec1fc992ae1c058dc0f363ef8d1e979
bb-28 <bb-28@MacBook-Pro-de-bb-28.local>
2020-09-15 01:02:21 +0000

    modification login,acceuil,traduction

--- src/app/views/pages/dashboard/dashboard.component.ts
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,24 @@
 // Angular
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'kt-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['dashboard.component.scss'],
+  selector: "kt-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styleUrls: ["dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
+  constructor(public router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onClick() {
+    this.router.navigate(["supprimer-article"]);
+  }
+
+  onClick2() {
+    this.router.navigate(["modifier-article"]);
+  }
+
+  onClick3() {
+    this.router.navigate(["modifier-article"]);
   }
 }
